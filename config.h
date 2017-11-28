e305910a211013693c34f28b4060d0215b4c2696
wert310 <310wert@gmail.com>
2017-11-28 16:40:26 +0100

    monoclegap option

--- config.h
+++ config.h
@@ -13,6 +13,7 @@ static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 5;        /* useless gap between windows */
+static const Bool monoclegap        = True;     /* show gap in monocle and single window mode */
 static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
 
--- dwm.c
+++ dwm.c
@@ -1366,7 +1366,7 @@ resizeclient(Client *c, int x, int y, int w, int h) {
 		gapincr = gapoffset = 0;
 	} else {
 		/* Remove border and gap if layout is monocle or only one client */
-		if (selmon->lt[selmon->sellt]->arrange == monocle || n == 1) {
+		if (!monoclegap && (selmon->lt[selmon->sellt]->arrange == monocle || n == 1)) {
 			gapoffset = 0;
 			gapincr = -2 * borderpx;
 			wc.border_width = 0;
